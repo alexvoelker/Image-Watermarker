e3652f47d23fce426ee3482d6564e1db524ff0f2
alexvoelker <alex@voelker.org>
2023-05-08 06:21:28 -0700

    added TODO's to main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tkinter as tk
+
+screen = tk.Tk()
+
+# TODO create tkinter GUI
+
+
+# TODO setup image input (watermark and image to be watermarked)
+
+
+# TODO add settings to tkinter GUI
+
+
+# TODO combine the image and watermakr based on user settings
+
+
+# TODO output watermarked file.
+
+
